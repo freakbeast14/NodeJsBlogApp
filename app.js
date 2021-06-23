f65422482c3a56478b1e6efa0f600e79d85234be
freakbeast14 <shahilmohammed14@gmail.com>
2021-06-23 16:45:47 +0530

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const blogRoutes = require("./routes/blogRoutes");
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 //express app
